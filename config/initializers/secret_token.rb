9aa226069800ac106385092ba73bb5169966ae94
Matt Buland <gik0geck0@gmail.com>
2013-03-24 09:23:54 -0600

    Created base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicTicketingSystem::Application.config.secret_token = '9609a38e430b780a437bb1093280521cd5e5ee846678277fabab93199814d7cd76ab0c9eddc4e43457a0b0905e930d2b1853f2db154305dd4336081dc89b4fb6'
