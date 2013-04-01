aa7353d0852655decb924fbdf543b69ecf2ab653
Matt Buland <gik0geck0@gmail.com>
2013-04-01 00:19:37 -0600

    Added user profile editability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
