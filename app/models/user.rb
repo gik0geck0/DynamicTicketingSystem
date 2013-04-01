a3fd287839768bee4ed4890db95a49d11f40f49d
Matt Buland <gik0geck0@gmail.com>
2013-03-31 21:36:04 -0600

    Add last name to users. Still profile ssl error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :password, :password_confirmation
-  attr_accessible :password, :password_confirmation, :email, :first_name
+  attr_accessible :password, :password_confirmation, :email, :first_name, :last_name
   before_save :encrypt_password
 
   validates_confirmation_of :password
