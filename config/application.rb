9de1d5eb3c8f983a52f8055f92205fe62c598281
Matt Buland <gik0geck0@gmail.com>
2013-04-24 00:17:23 -0600

    Setup some box model look

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module DynamicTicketingSystem
   class Application < Rails::Application
-    config.force_ssl = false
+    #config.force_ssl = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
