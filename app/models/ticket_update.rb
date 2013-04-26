c8172480cb74643f3c1cb139eb3ac90a8e1db43b
Matt Buland <gik0geck0@gmail.com>
2013-04-26 10:24:18 -0600

    Comment stuff out

--- app/models/ticket_update.rb
+++ app/models/ticket_update.rb
@@ -5,12 +5,12 @@ class Ticket < ActiveRecord::Base
   #has_one :user, as: :owner
   #has_one :user, as: :updater
 
-  validates_presence_of :updater, :ticket, :revision, :description
+  #validates_presence_of :updater, :ticket, :revision, :description
 
   # Status and owner are optional, required only to show a change
   # time_logged would otherwise be 0, if there's 'no change'
   # Revision needs to be auto-incremented, but localized per ticket (which would be tougher in the DB than in rails)
 
-  attr_accessible :updater, :ticket, :revision, :description, :time_logged
+  #attr_accessible :updater, :ticket, :revision, :description, :time_logged
 
 end
