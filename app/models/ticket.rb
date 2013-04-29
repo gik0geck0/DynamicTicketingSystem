6637a535ee43521007064691eab634e467e4a0cc
Matt Buland <gik0geck0@gmail.com>
2013-04-29 17:23:19 -0600

    Methodize finding the current ticket parameters

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -10,4 +10,30 @@ class Ticket < ActiveRecord::Base
   attr_accessor :uid, :sid
   attr_accessible :description, :name, :uid, :sid, :user, :status
 
+  def get_current_status
+    set_latest_update
+
+    if @latest_update
+      return @latest_update.status
+    else
+      return self.status
+    end
+  end
+
+  def get_current_owner
+    set_latest_update
+
+    if @latest_update
+      return @latest_update.owner
+    else
+      return self.user
+    end
+  end
+
+  def set_latest_update
+    if not @latest_update
+      @latest_update = TicketUpdate.where(["ticket_id = ?", self.id]).order("created_at").last
+    end
+  end
+
 end
